b19cefdc93f630c99f40c1b1bda106d5e1c58442
cph <chewph@gmail.com>
2017-06-09 17:33:33 +0800

    Created http-client folder with ng scaffolded files

---
+++ src/app/http-client/http-client.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-http-client',
+  template: `
+    <p>
+      http-client Works!
+    </p>
+  `,
+  styles: []
+})
+export class HttpClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
