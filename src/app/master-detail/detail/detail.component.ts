19e096f6a071762f8db797ebbfd361ff77f74dff
cph <chewph@gmail.com>
2017-07-10 13:22:31 +0800

    Removed DetailComponent css and html

--- src/app/master-detail/detail/detail.component.ts
+++ src/app/master-detail/detail/detail.component.ts
@@ -5,17 +5,16 @@ import { Hero } from '../../classes/hero';
 @Component({
 	selector: 'detail',
 	template: `<div *ngIf="heroDetail; else showEmpty">
-					<h3>Detail - DetailComponent</h3>
-					<div><label>id: </label>{{ heroDetail.id }}</div>
-					<div><label>name: </label>
-						<input [(ngModel)]="heroDetail.name" placeholder="name">
-					</div>
+				<h3>Detail - DetailComponent</h3>
+				<div><label>id: </label>{{ heroDetail.id }}</div>
+				<div><label>name: </label>
+					<input [(ngModel)]="heroDetail.name" placeholder="name">
 				</div>
+			</div>
 
-				<ng-template #showEmpty>No hero selected</ng-template>`
+			<ng-template #showEmpty>No hero selected</ng-template>`
 })
 export class DetailComponent implements OnInit{
-	//@Input('heroesArrInput') heroesDetailArr: Hero;
 	@Input('heroDetailInput') heroDetail: Hero; // From MasterComponent
 
 	constructor(){
